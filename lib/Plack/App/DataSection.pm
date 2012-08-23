dbee0507161bb85612d49f4945c6fa430be944de
Masayuki Matsuki <y.songmu@gmail.com>
2012-08-23 16:19:03 +0000

    Import of SONGMU/Plack-App-DataSection-0.01 from CPAN.
    gitpan-cpan-distribution: Plack-App-DataSection
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SONGMU/Plack-App-DataSection-0.01.tar.gz
    gitpan-cpan-author:       SONGMU
    gitpan-cpan-maturity:     released

---
+++ lib/Plack/App/DataSection.pm
@@ -0,0 +1,150 @@
+package Plack::App::DataSection;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+use parent qw/Plack::Component/;
+use MIME::Base64;
+use Data::Section::Simple;
+use Plack::MIME;
+use HTTP::Date;
+
+use Plack::Util::Accessor qw(encoding);
+
+sub call {
+    my $self = shift;
+    my $env  = shift;
+
+    my $path = $env->{PATH_INFO} || '';
+    if ($path =~ /\0/) {
+        return $self->return_400;
+    }
+    $path =~ s!^/!!;
+
+    my ($data, $content_type) = $self->get_content($path);
+
+    return $self->return_404 unless $data;
+
+    return [ 200, [
+        'Content-Type'   => $content_type,
+        'Content-Length' => length($data),
+        'Last-Modified'  => $self->last_modified,
+    ], [ $data ] ];
+}
+
+sub return_400 {
+    my $self = shift;
+    return [400, ['Content-Type' => 'text/plain', 'Content-Length' => 11], ['Bad Request']];
+}
+
+sub return_404 {
+    my $self = shift;
+    return [404, ['Content-Type' => 'text/plain', 'Content-Length' => 9], ['not found']];
+}
+
+sub data_section {
+    my $self = shift;
+
+    $self->{_reader} ||= Data::Section::Simple->new(ref $self);
+}
+
+sub get_data_section {
+    my ($self, $path) = @_;
+    $self->{_data_section_hash} ||= $self->data_section->get_data_section;
+    $self->{_data_section_hash}{$path};
+}
+
+sub _cache { shift->{_cache} ||= {} }
+
+sub get_content {
+    my ($self, $path) = @_;
+
+    my $mime_type = Plack::MIME->mime_type($path);
+    my $is_binary = is_binary($mime_type);
+
+    unless ($is_binary) {
+        my $encoding = $self->encoding || 'utf-8';
+        $mime_type .= "; charset=$encoding;";
+    }
+
+    my $content = $self->_cache->{$path} ||= do {
+        my $content = $self->get_data_section($path);
+        $content = decode_base64($content) if $content && $is_binary;
+        $content;
+    };
+
+    ($content, $mime_type);
+}
+
+sub is_binary {
+    my $mime_type = shift;
+
+    $mime_type !~ /\b(?:text|xml|javascript|json)\b/;
+}
+
+sub last_modified {
+    my $self = shift;
+
+    $self->{last_modified} ||= do {
+        my $mod = ref $self;
+        $mod =~ s!::!/!g;
+        $mod .= '.pm';
+        my $full_path = $INC{$mod};
+
+        my @stat = stat $full_path;
+        HTTP::Date::time2str( $stat[9] )
+    };
+}
+
+1;
+__DATA__
+@@ sample.txt
+さんぷる
+
+__END__
+
+=head1 NAME
+
+Plack::App::DataSection - psgi application for serving contents in data section
+
+=head1 SYNOPSIS
+
+  # create your module from directory.
+  % dir2data_section.pl --dir=dir/ --module=Your::Module
+
+  # generated module is like this.
+  package Your::Module;
+  use parent qw/Plack::App::DataSection/;
+  __DATA__
+  @@ index.html
+  <html>
+  ...
+
+  # app.psgi
+  use Your::Module;
+  Your::Module->new->to_app;
+
+  # you can get contents in data section
+  % curl http://localhost:5000/index.thml
+
+=head1 DESCRIPTION
+
+Plack::App::DataSection is psgi application for serving contents in data section.
+
+Inherit this module and you can easly create psgi application for serving contents in data section.
+
+You can even serve binary contents!
+
+
+=head1 AUTHOR
+
+Masayuki Matsuki E<lt>y.songmu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/01_data_section.t
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+use Test::More;
+use HTTP::Request::Common;
+use HTTP::Response;
+use Plack::Test;
+use Plack::App::DataSection;
+
+my $handler = Plack::App::DataSection->new;
+
+my %test = (
+    client => sub {
+        my $cb  = shift;
+
+        my $res = $cb->(GET "/sample.txt");
+        like $res->content, qr/さんぷる/;
+    },
+    app => $handler,
+);
+
+test_psgi %test;
+
+done_testing;
